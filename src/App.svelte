be25e1c0c99596ad6a850a4be32dc351f069a931
Szabolcs Molnár <szabolcsmolnar82@gmail.com>
2024-11-01 10:11:09 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+
+  class Temperature {
+    #c = $state(0)
+    #f = $state(0)
+
+    get c() {
+      return this.#c
+    }
+
+    set c(c) {
+      this.#c = c
+      this.#f = c * 9 / 5 + 32
+    }
+
+    get f() {
+      return this.#f
+    }
+
+    set f(f) {
+      this.#c = (f - 32) * 5 / 9
+    }
+  }
+
+  const temperature = new Temperature()
+
+
+
+</script>
+
+<div>
+  <input type="number" bind:value={temperature.c} />Celsius=
+  <input type="number" bind:value={temperature.f} />Fahrenheit
+</div>
+
+<style>
+  div {
+    font-size: 30px;
+    color: white;
+  }
+  input[type="number"] {
+    max-width: 10ch;
+    text-align: center;
+    font-size: 25px;
+    font-family: 'Times New Roman', Times, serif;
+    border-radius: 30px;
+  }
+</style>
